5861ab303e3d9d56e36a17609bc7d9ca43b95b4d
nimje.p@northeastern.edu <nimje.p@northeastern.edu>
2024-04-03 12:08:10 -0400

    Implemented changes for verify email api to be accessed on https

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,30 +1,7 @@
 const { Sequelize } = require("sequelize");
-const { Connector } = require("@google-cloud/cloud-sql-connector");
 const dotenv = require("dotenv");
 
 dotenv.config();
-// const cloudSqlSequelize = async () => {
-//   const connector = new Connector();
-//   const clientOpts = await connector.getOptions({
-//     instanceConnectionName: process.env.DB_CONNECTION_NAME,
-//     ipType: 'PRIVATE',
-//   });
-
-//   // Create a Sequelize instance using the connection options from the Cloud SQL Connector
-//   const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//     host: clientOpts.host,
-//     dialect: 'postgres',
-//     logging: false,
-//     pool: {
-//       max: 10,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     },
-//   });
-
-//   return sequelize;
-// };
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
